3d57c268b52abb6641b91c183bcee0d77e892600
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2021-12-21 20:09:22 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcRectPerimeter() {
+    const a = prompt('Input first side of a rectangle')
+    const b = prompt('Input second side of a rectangle')
+    const perimeter = a * 2 + b * 2
+    alert('The perimeter of this rectangle is ' + perimeter)
+}
+
+calcRectPerimeter()
